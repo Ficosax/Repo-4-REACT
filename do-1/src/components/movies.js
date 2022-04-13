90b122d32b6547ff0b6159dd3f89c05d6d300150
Ficosax <filipsaxmkd@gmail.com>
2022-04-14 01:03:04 +0200

    dom1

---
+++ do-1/src/components/movies.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const movies = ({ listaNaFilmovi }) => {
+    return(
+        <div id='movies'>
+            <h2>Movies:</h2>
+            <ol>
+                {listaNaFilmovi.map((filmovi, i) => {
+                    return(
+                        <li key={i}>
+                            <p>Name: {filmovi.name}</p>
+                            <p>Release: {filmovi.date}</p>
+                            <p>Genre: {filmovi.genre}</p>
+                            <p>IMDbLink: {filmovi.imdbLink}</p>
+                            <p>ImgUrl: {filmovi.imgUrl}</p>
+                        </li>
+                    )
+                })}
+            </ol>
+        </div>
+    )
+}
